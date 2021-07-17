e4ccbfb4ce787f922435bc67558253c8db7f00ea
Escribe tu nombre sin tildes, y letras como ø, ñ <sergiotrubio@gmail.com>
2021-07-18 01:15:48 +0200

    Segundo método

--- prueba.rb
+++ prueba.rb
@@ -13,3 +13,19 @@ def request(address)
     data = JSON.parse(response.read_body)
     data
 end
+
+def build_web_page
+    hash_rover = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=10&api_key=xeWWb56qSwafBBq86t2RuCMg63i3hfCrSt8F3VQo")
+    m = hash_rover["photos"].count
+    lista = ""
+    m.times do |f|
+        photo = hash_rover["photos"][f]["img_src"]
+        lista += "\t\t<li><img width='250' height='300' src=\"#{photo}\"></li>\n"
+    end
+    puts m
+result = "<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body>\n\t<ul>\n#{lista}\t</ul>\n</body>\n</html>"
+end
+
+pagina = build_web_page
+
+File.write('index.html',pagina)
